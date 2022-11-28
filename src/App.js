057ea661cd302a7fb5105925b1acb51eb7ba389a
Stefano Casasola <ste.casasola@gmail.com>
2022-11-28 13:48:47 +0100

    MovieForm is now a separate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Component } from 'react'
-import { Col, Container, Form, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import MovieCard from './components/MovieCard'
+import MovieForm from './components/MovieForm'
 
 class App extends Component {
   state = {
     selectedMovieTitle: 'Doctor Strange',
+  }
+
+  changeSelectedMovieTitle = (newMovieTitle) => {
+    this.setState({
+      selectedMovieTitle: newMovieTitle,
+    })
   }
 
   render() {
@@ -16,26 +23,10 @@ class App extends Component {
           <Row className="justify-content-center mt-4">
             <Col xs={12} md={6}>
               <h1>Movie Info App!</h1>
-              <Form>
-                <Form.Group>
-                  <Form.Label>Choose your movie here</Form.Label>
-                  <Form.Control
-                    as="select"
-                    value={this.state.selectedMovieTitle}
-                    onChange={(e) =>
-                      this.setState({
-                        selectedMovieTitle: e.target.value,
-                      })
-                    }
-                  >
-                    <option>Doctor Strange</option>
-                    <option>Ironman</option>
-                    <option>Black Widow</option>
-                    <option>The Avengers</option>
-                    <option>The Hulk</option>
-                  </Form.Control>
-                </Form.Group>
-              </Form>
+              <MovieForm
+                selectedMovieTitle={this.state.selectedMovieTitle} // READS THE VALUE
+                changeSelectedMovieTitle={this.changeSelectedMovieTitle} // CHANGES THE VALUE
+              />
             </Col>
           </Row>
           <Row className="justify-content-center mt-4">
